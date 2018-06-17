d99a41d393eaf4b8d3fa55dc6fc79acc2ebec14d
Eduardo Velasco <eduardo@ordermygear.com>
2018-06-17 00:03:28 -0500

    set up project

---
+++ frontend/src/App.js
@@ -0,0 +1,23 @@
+import React, { Component } from 'react'
+
+class App extends Component {
+  render () {
+    return (
+      <div className="container">
+        <div className="row">
+          <div className="col-sm">
+            One of three columns
+          </div>
+          <div className="col-sm">
+            One of three columns
+          </div>
+          <div className="col-sm">
+            One of three columns
+          </div>
+        </div>
+      </div>
+    )
+  }
+}
+
+export default App
